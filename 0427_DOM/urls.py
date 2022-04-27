1625ddd83c525e24252df13139db3050985f949c
eduyu <neo@hphk.kr>
2022-04-27 16:02:02 +0900

    0427

---
+++ 0427_DOM/urls.py
@@ -0,0 +1,19 @@
+# urls.py
+
+
+def 로제(dduk, source, sari):
+    if request.method == 'POST':
+        pass
+
+
+알바생이만든다('로제용/', views.index)
+
+'url articles/ 가 들어오면, 대기하다 views.index 함수를 실행한다.'
+
+
+import random
+
+# numbers = range(1, 46)
+# random.sample(numbers, 6)
+
+random.sample(range(1, 46), 6)
