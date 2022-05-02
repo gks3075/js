e1c8d053f9fe22adeccd968a3831e374e0ad6a5a
eduyu <neo@hphk.kr>
2022-05-02 16:05:14 +0900

    0502

--- 0502_AJAX/part1/03_callback.py
+++ 0502_AJAX/part1/03_callback.py
@@ -1,4 +1,4 @@
-# 1. Pass function to argument
+#1. Pass function to argument
 def my_func(func):
     return func
 
@@ -8,8 +8,7 @@ def my_argument_func():
 result = my_func(my_argument_func)
 print(result())
 
-
-# 2. map
+#2. map
 numbers = [0, 9, 99]
 
 def plus_one(number):
